c1d556377a56c71f3a1d968ddc57653ff085cfa8
kostilal <i.kostyukevich@gmail.com>
2020-04-06 18:24:36 +0300

    Initial Commit

---
+++ kostilalMathLib/kostilalMathLib.h
@@ -0,0 +1,19 @@
+//
+//  kostilalMathLib.h
+//  kostilalMathLib
+//
+//  Created by Илья Костюкевич on 06.04.2020.
+//  Copyright © 2020 Ilya Kostyukevich. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for kostilalMathLib.
+FOUNDATION_EXPORT double kostilalMathLibVersionNumber;
+
+//! Project version string for kostilalMathLib.
+FOUNDATION_EXPORT const unsigned char kostilalMathLibVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <kostilalMathLib/PublicHeader.h>
+
+

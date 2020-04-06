1be3a2e11e625ecc6b4e4e373cda9d5465307e8f
kostilal <i.kostyukevich@gmail.com>
2020-04-06 18:31:26 +0300

    Initial project setup

---
+++ kostilalMathLib/KostilalMath.swift
@@ -0,0 +1,21 @@
+//
+//  KostilalMath.swift
+//  kostilalMathLib
+//
+//  Created by Илья Костюкевич on 06.04.2020.
+//  Copyright © 2020 Ilya Kostyukevich. All rights reserved.
+//
+
+import Foundation
+
+final class KostilalMath {
+    public init() {}
+    
+    public func substruct(a: Int, b: Int) -> Int {
+        return a - b
+    }
+    
+    public func divide(a: Int, b: Int) -> Int {
+        return a / b
+    }
+}
